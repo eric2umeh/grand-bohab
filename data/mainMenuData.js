0c08b01b3c3a80870cd1af5f2eb832438af3d65e
Eric <eric2umeh@yahoo.com>
2023-09-23 16:51:41 +0100

    Create data

---
+++ data/mainMenuData.js
@@ -0,0 +1,94 @@
+export const homeItems = [
+  {
+    name: "Home 01",
+    routePath: "/",
+  },
+  {
+    name: "Home 02",
+    routePath: "/home/home_2",
+  },
+  {
+    name: "Home 03",
+    routePath: "/home/home_3",
+  },
+  {
+    name: "Home 04",
+    routePath: "/home/home_4",
+  },
+  {
+    name: "Home 05",
+    routePath: "/home/home_5",
+  },
+  {
+    name: "Home 06",
+    routePath: "/home/home_6",
+  },
+  {
+    name: "Home 07",
+    routePath: "/home/home_7",
+  },
+  {
+    name: "Home 08",
+    routePath: "/home/home_8",
+  },
+  {
+    name: "Home 09",
+    routePath: "/home/home_9",
+  },
+  {
+    name: "Home 10",
+    routePath: "/home/home_10",
+  },
+];
+export const blogItems = [
+  {
+    name: "Blog List V1",
+    routePath: "/blog/blog-list-v1",
+  },
+  {
+    name: "Blog List V2",
+    routePath: "/blog/blog-list-v2",
+  },
+  {
+    name: "Blog Single",
+    routePath: "/blog/blog-details/1",
+  },
+];
+export const pageItems = [
+  {
+    name: "404",
+    routePath: "/404",
+  },
+  {
+    name: "About",
+    routePath: "/others-pages/about",
+  },
+  {
+    name: "Become Expert",
+    routePath: "/others-pages/become-expert",
+  },
+  {
+    name: "Help Center",
+    routePath: "/others-pages/help-center",
+  },
+  {
+    name: "Login",
+    routePath: "/others-pages/login",
+  },
+  {
+    name: "Register",
+    routePath: "/others-pages/signup",
+  },
+  {
+    name: "Terms",
+    routePath: "/others-pages/terms",
+  },
+  {
+    name: "Invoice",
+    routePath: "/others-pages/invoice",
+  },
+  {
+    name: "UI Elements",
+    routePath: "/others-pages/ui-elements",
+  },
+];
